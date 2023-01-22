ea4a4a967b79e28898a5951459204afe8f0114c2
Gama Mac <nahalielgamaliel@hotmail.com>
2023-01-21 20:05:06 -0600

    Standard Styles

---
+++ app/components/EditButton.jsx
@@ -0,0 +1,7 @@
+import styles from '../../styles/components/editButton.module.css'
+
+export function EditButton() {
+    return(
+        <button className={styles.editbutton}>Editar</button>
+    );
+}
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import { EditButton } from "../components/EditButton.jsx";
+
+export default function Dashboard() {
+    return(
+        <div>
+        <h1>dashboard 2</h1>
+        <EditButton />
+        </div>
+
+    );
+}
