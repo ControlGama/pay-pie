3cf2e6e69648dc10293ae7a8d5ef0ee634714317
Gama Mac <nahalielgamaliel@hotmail.com>
2023-02-19 17:48:09 -0600

    Dashboard Container

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,19 @@
 import { DashboardCard } from '../containers/DashboardCard';
 
+import containerStyles from "../../styles/containers.module.css";
+
+
 export default function Dashboard() {
     return(
-        <div>
-        <DashboardCard />
+        <div className={containerStyles.dashboard_container}>
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
+            <DashboardCard />
         </div>
 
     );
