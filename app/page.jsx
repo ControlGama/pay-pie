6246ca273970bc57f47585c95ed65d09cf99f567
ControlGama <49002290+ControlGama@users.noreply.github.com>
2023-02-26 20:23:13 -0600

    [Add] firebase conection

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,16 @@
+import { FirebaseApp } from "./firebase/firebase"
+import  {getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc} from "firebase/firestore"
+
+
 export default function WelcomePage() {
+    
+    
+    const db = getFirestore(FirebaseApp);
+    const docRef = doc(db, 'pagos', 'M9MMwjc9jtyjUsgZLUp0')
+    const pago = getDoc(docRef)
+    // const pago = docSnap.data()
+    console.log(pago);
+
     return(
         <h1>HOOOO</h1>
     )
